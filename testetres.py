f8654cc2bf389c16dc78cb9603e26178423a8a9c
Laura <laurapapa693@gmail.com>
2025-09-15 10:36:59 -0300

    add teste

--- testetres.py
+++ testetres.py
@@ -1,5 +1,4 @@
 import requests
-
 url = "https://api.adviceslip.com/advice"
 
 resposta = requests.get(url)
@@ -8,5 +7,27 @@ if resposta.status_code == 200:
     dados = resposta.json()
     conselho = dados['slip']['advice']
     print("💡 Conselho do dia:", conselho)
+else:
+    print("Erro:", resposta.status_code, resposta.text)
+
+
+APP_ID = "e0648809"
+APP_KEY = "197ccb154919c7c526bdb8a201b34a7c"
+
+url = "https://api.edamam.com/api/nutrition-data"
+
+params = {
+    "app_id": APP_ID,
+    "app_key": APP_KEY,
+    "ingr": "1 large apple"
+}
+
+resposta = requests.get(url, params=params)
+
+if resposta.status_code == 200:
+    dados = resposta.json()
+    print("Calorias:", dados.get("calories"))
+    print("Peso total:", dados.get("totalWeight"))
+    print("Nutrientes:", dados.get("totalNutrients"))
 else:
     print("Erro:", resposta.status_code, resposta.text)
